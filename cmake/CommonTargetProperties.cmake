a383f643955ef72c34164cf19c35d97525d01e0a
Milan Hanus <mhanus@kma.zcu.cz>
2011-07-21 13:31:00 +0200

    Add optimization flag.

--- cmake/CommonTargetProperties.cmake
+++ cmake/CommonTargetProperties.cmake
@@ -4,6 +4,10 @@ macro(SET_COMMON_TARGET_PROPERTIES TRGT HERMES_VERSION)
 		
         find_package(HERMES REQUIRED)
 
+  if (COMPILE_FLAGS)
+    set_target_properties(${TRGT} PROPERTIES COMPILE_FLAGS ${COMPILE_FLAGS})
+  endif (COMPILE_FLAGS)
+
 	target_link_libraries(${TRGT} ${HERMES_COMMON_LIBRARY})
 	target_link_libraries(${TRGT} ${HERMES_LIBRARY})
 	# Is empty if WITH_TRILINOS = NO
