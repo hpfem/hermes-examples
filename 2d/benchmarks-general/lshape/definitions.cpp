32902cc7317c32718d9fa0eee3a841a5de5fd9d0
Pavel Solin <solin.pavel@gmail.com>
2011-07-22 09:29:07 -0400

    Benchmark lshape added.

---
+++ 2d/benchmarks-general/lshape/definitions.cpp
@@ -0,0 +1,22 @@
+#include "definitions.h"
+
+double CustomExactSolution::value(double x, double y) const 
+{
+  double r = Hermes::sqrt(x*x + y*y);
+  double a = Hermes::atan2(x, y);
+  return Hermes::pow(r, 2.0/3.0) * Hermes::sin(2.0*a/3.0 + M_PI/3);
+}
+
+void CustomExactSolution::derivatives(double x, double y, double& dx, double& dy) const 
+{
+  double t1 = 2.0/3.0*Hermes::atan2(x, y) + M_PI/3;
+  double t2 = Hermes::pow(x*x + y*y, 1.0/3.0);
+  double t3 = x*x * ((y*y)/(x*x) + 1);
+  dx = 2.0/3.0*x*Hermes::sin(t1)/(t2*t2) + 2.0/3.0*y*t2*Hermes::cos(t1)/t3;
+  dy = 2.0/3.0*y*Hermes::sin(t1)/(t2*t2) - 2.0/3.0*x*t2*Hermes::cos(t1)/t3;
+}
+
+Ord CustomExactSolution::ord(Ord x, Ord y) const 
+{
+  return Ord(20);
+}
---
+++ 2d/benchmarks-general/lshape/definitions.h
@@ -0,0 +1,21 @@
+#include "hermes2d.h"
+
+using namespace Hermes::Hermes2D;
+using namespace WeakFormsH1;
+using Hermes::Ord;
+
+/*  Exact solution */
+
+class CustomExactSolution : public ExactSolutionScalar<double>
+{
+public:
+  CustomExactSolution(Mesh* mesh) : ExactSolutionScalar(mesh) 
+  {
+  }
+
+  virtual double value(double x, double y) const;
+
+  virtual void derivatives(double x, double y, double& dx, double& dy) const;
+
+  virtual Ord ord(Ord x, Ord y) const;
+};
