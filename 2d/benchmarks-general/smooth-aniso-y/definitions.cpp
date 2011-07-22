b9a450686e91a4a4c1dae2a966a4ac90c6000f40
Pavel Solin <solin.pavel@gmail.com>
2011-07-22 18:30:49 -0400

    Adding benchmark smooth-aniso-y.

---
+++ 2d/benchmarks-general/smooth-aniso-y/definitions.cpp
@@ -0,0 +1,40 @@
+#include "definitions.h"
+
+void CustomExactSolution::derivatives(double x, double y, double& dx, double& dy) const 
+{
+  dx = 0;
+  dy = Hermes::cos(y);
+}
+
+double CustomExactSolution::value(double x, double y) const 
+{
+  return Hermes::sin(y);
+}
+
+Ord CustomExactSolution::ord(Ord x, Ord y) const 
+{
+  return Ord(7);
+}
+
+
+double CustomFunction::value(double x, double y) const 
+{
+  return -Hermes::sin(y);
+}
+
+Ord CustomFunction::value(Ord x, Ord y) const 
+{
+  return Ord(7);
+}
+
+
+CustomWeakFormPoisson::CustomWeakFormPoisson(std::string bdy_marker_top) : WeakForm(1) 
+{
+  // Jacobian.
+  add_matrix_form(new DefaultJacobianDiffusion<double>(0, 0));
+
+  // Residual.
+  add_vector_form(new DefaultResidualDiffusion<double>(0));
+  add_vector_form(new DefaultVectorFormVol<double>(0, Hermes::HERMES_ANY, new CustomFunction));
+  add_vector_form_surf(new DefaultVectorFormSurf<double>(0, bdy_marker_top, new HermesFunction<double>(1.0)));
+}
