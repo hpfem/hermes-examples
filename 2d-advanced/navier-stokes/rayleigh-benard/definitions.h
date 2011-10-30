808d8c30741cb0540557903c6504479e3520e309
Lukas Korous <lukas.korous@gmail.com>
2011-10-30 21:57:33 +0100

    !!! IMPORTANT !!!
    Delete examples reported as not working from master.
    
    !!! IMPORTANT !!!

--- 2d-advanced/navier-stokes/rayleigh-benard/definitions.h
+++
@@ -1,9 +0,0 @@
-#include "hermes2d.h"
-
-/* Namespaces used */
-
-using namespace Hermes;
-using namespace Hermes::Hermes2D;
-using namespace Hermes::Hermes2D::Views;
-using namespace Hermes::Hermes2D::RefinementSelectors;
-using namespace Hermes::Hermes2D::WeakFormsH1;
