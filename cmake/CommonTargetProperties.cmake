6ce20d1a28ff8d7cb0469fa172dcad810d142302
Lukas Korous <lukas.korous@gmail.com>
2016-09-30 21:43:35 +0200

    Fixes with CMake.

--- cmake/CommonTargetProperties.cmake
+++ cmake/CommonTargetProperties.cmake
@@ -1,10 +1,10 @@
 macro(SET_COMMON_TARGET_PROPERTIES TRGT HERMES_VERSION)
 	set(HERMES_VERSION ${HERMES_VERSION})
-  find_package(HERMES REQUIRED)
+	find_package(HERMES REQUIRED)
 
 	target_link_libraries(${TRGT} ${HERMES_COMMON_LIBRARY})
 	target_link_libraries(${TRGT} ${HERMES_LIBRARY} ${MATIO_LIBRARY} ${BSON_LIBRARY})
-  target_link_libraries(${TRGT} ${TESTING_CORE_LIBRARY})
+	target_link_libraries(${TRGT} ${TESTING_CORE_LIBRARY})
 
 	# Is empty if WITH_TRILINOS = NO
 	target_link_libraries(${TRGT} ${TRILINOS_LIBRARIES})
